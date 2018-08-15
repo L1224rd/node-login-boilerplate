b09d798ec597b6d98dd90d307ae90fb699ed89cb
Eder Marques <eder.marques@citi.org.br>
2018-08-15 20:33:00 -0300

    saving before add webpack to project

--- app.js
+++ app.js
@@ -35,7 +35,11 @@ app.use(express.static('views'));
 
 // ==================== DATABASE ==================== //
 
-mongoose.connect('mongodb://127.0.0.1/citiponto');
+mongoose.connect(
+  // connect to database runing at the specified port
+  'mongodb://localhost:27017/citiponto',
+  { useNewUrlParser: true },
+);
 
 // Get Mongoose to use the global promise library
 mongoose.Promise = global.Promise;
@@ -46,6 +50,10 @@ const db = mongoose.connection;
 // Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+// ==================== FUNCTIONS ==================== //
+
+const getViewPath = view => path.join(__dirname, `views/${view}/${view}.html`);
+
 // ==================== ROUTES ==================== //
 
 app.get('/', (req, res) => {
@@ -53,11 +61,11 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/login/login.html'));
+  res.sendFile(getViewPath('login'));
 });
 
 app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/home/home.html'));
+  res.sendFile(getViewPath('home'));
 });
 
 // ==================== START SERVER ==================== //
