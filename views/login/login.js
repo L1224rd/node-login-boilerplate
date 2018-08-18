e051ffca3779561ecfbbc2e847c7c3a958ff2c02
Eder Marques <eder.marques@citi.org.br>
2018-08-17 21:10:20 -0300

    add login page

--- views/login/login.js
+++ views/login/login.js
@@ -1,1 +1,7 @@
-console.log('yay');
+$('document').ready(() => {
+  $('#no-password').click(() => {
+    $('#pass2').css('display', 'block'); // input pass2 shows up
+    $('input[value=Login]').attr('value', 'Salvar'); // changes the value from the submit button
+    $('#login').attr('action', 'http://localhost:3000/users/password'); // changes the action of the post to receive the new password
+  });
+});
